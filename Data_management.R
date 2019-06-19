b1cdfc6f716c0679bc32169dbc9288d0b6b66740
Oscar E. R. Lehmann <lehmanncxii@gmail.com>
2019-06-19 15:57:08 -0300

    Minor modifications. WIP.

--- Data_management.R
+++ Data_management.R
@@ -1,3 +1,36 @@
-# Data management.
+##### DATA MANAGEMENT. #########################################################
+
+# rm(list = ls())
+
+##### File management and importation. #########################################
 
 library("dplyr")
+
+FileNames <- dir("All_matrices")
+
+MatrixNames <- FileNames %>% 
+  gsub(pattern = ".nex", replacement = "", x = .) %>% 
+  gsub(pattern = "_", replacement = " ", x = .)
+
+# Import the matrices with Claddis.
+
+library("Claddis")
+
+x <- 0
+
+for (i in seq_along(FileNames)) {
+  
+  tryCatch(ReadMorphNexus(paste0("All_matrices/", FileNames[i])),
+           
+           error = function (err) {
+             
+             x <<- append(x = x, values = i)
+             
+           },
+           
+           finally = next)
+  
+}
+
+x <- x[-1]
+
